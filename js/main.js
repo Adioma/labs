d2e21dda3fa4f4e3524d91ae0e113e8a45fd8408
iamanna <anna@fnf.vc>
2014-01-31 00:24:25 +0800

    First draft of page: modal contact form, examples, selling points

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$('#imagePrevModal').on('show.bs.modal', function (e) {
 
+	$('<img class="img-responsive" src="'+ e.relatedTarget.dataset.remoteImage+ '">').load(function() {
+		  $('#imagePrevModal .modal-body').html('');
+	      $(this).appendTo($('#imagePrevModal .modal-body'));
+	});
+});
