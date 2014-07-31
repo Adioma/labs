030041b9b9992f4853cb4ba05c93f44caf1b375a
Mark Vital <markvital@Marks-MacBook-Pro.local>
2014-07-31 10:43:47 +0300

    Updated contact form

--- js/main.js
+++ js/main.js
@@ -4,4 +4,26 @@ $('#imagePrevModal').on('show.bs.modal', function (e) {
 		  $('#imagePrevModal .modal-body').html('');
 	      $(this).appendTo($('#imagePrevModal .modal-body'));
 	});
+	
+});
+
+
+$( document ).ready(function() {
+  $('#contact-us').bootstrapValidator();
+
+  $('#do-contact').click(function() {
+    //if ($('#contact-us').isValid() ) {
+      var subject = $( "input[name=company]" ).val() + " [Infographic]";
+      var body = "Hi,\n I would like to make an infographic. " +
+                 "Here is my infographic project description:" + "\n" + $("textarea[name=description]" ).val() +
+                 "\nBudget: " + $( "select[name=budget]" ).val() + "\n\n" +
+                 $("input[name=name]" ).val() + "\n" +
+                 $("input[name=phone]" ).val();
+      $(location).attr('href', 'mailto:?subject='
+                               + encodeURIComponent(subject)
+                               + "&body=" 
+                               + encodeURIComponent(body)
+      );
+    //}
+  });
 });
